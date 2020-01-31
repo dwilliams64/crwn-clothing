8fa09cd6efb01718a4adf99db2932ad381508575
dwilliams64 <dwilliams_64@yahoo.com>
2020-01-31 11:30:16 -0600

    Code clean up

--- src/App.js
+++ src/App.js
@@ -1,39 +1,13 @@
 import React from 'react';
 import {Route, Switch} from 'react-router-dom';
-
-import './App.css';
-
 import HomePage from './pages/homepage/homepage.component';
 import HatsPage from './pages/hatspage/hatspage.component';
-
-// Commit change
-
-
+import './App.css';
 
 function App() {
   return (
     <div>
-
       <Switch>
-        {/* Currently we want to access history from our Route components prop and
-        use it for our MenuItem component so when a user clicks on the MenuItem
-        that page will be rendered.
-
-        The problem is that the MenuItem component is a child of the HomePage component.
-
-        history can only be accessed by the parent component which is HomePage in this case.
-
-        We could pass down the history prop from the HomePage component and then pass it to the
-        child component which is the MenuItem component in this case, but this is a bad practice 
-        called prop drilling/prop tunneling and could become a problem later on as our application expands.
-
-        Luckly for us react-router-dom has higher order component called withRouter() that can help us 
-        out.
-
-        A higher order compoent in React is a function that takes a component and returns a modified 
-        component. Go to the MenuItem component to see how it works and for more details.  */}
-
-
         <Route exact path='/' component={HomePage} />
         <Route path='/hats' component={HatsPage} />
       </Switch>
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -18,31 +18,10 @@ class Directory extends React.Component {
        
         return (
             <div className='directory-menu'>
-                { /* Before spread operator
-
-                    data.map(({id, title, imageUrl, size}) => {                        
-                        return <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
-                    })  
-
-                    /* After spread operator. */
-
-                    /* The spread operator allows use to set the rest of the props on our component
-                        without typing in each prop one by one.
-
-                        This can save on space and makes for much cleaner code when have to many props.
-
-                        The ...otherSectoinProps will populate the other props we didn't manually deconstruct (like id to be used in key)
-                        and place them as props using the property from our object as the prop and the value as the prop value.
-
-                        https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax
-
-                    */
-
+                {
                     data.map(({id, ...otherSectionProps}) => {
                         return <MenuItem key={id} {...otherSectionProps} />
                     })
-
-
                 } 
                 
             </div>
