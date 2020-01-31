04cc0344bcb52f1b94ef9aac344767cd8c084b7d
dwilliams64 <dwilliams_64@yahoo.com>
2020-01-31 10:41:49 -0600

    Resolving conflict

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import {Route, Switch} from 'react-router-dom'
-import Homepage from './pages/homepage/homepage.component';
+
+import HomePage from './pages/homepage/homepage.component';
 
 function App() {
   return (
     <div>
-      <Homepage />
+      
     </div>
   );
 }
