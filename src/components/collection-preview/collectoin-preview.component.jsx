dfbfd2712fcfc9e3bc947ec163b25ac3b579734e
dwilliams64 <dwilliams_64@yahoo.com>
2020-02-03 10:00:23 -0600

    Removed comments

--- src/components/collection-preview/collectoin-preview.component.jsx
+++ src/components/collection-preview/collectoin-preview.component.jsx
@@ -6,14 +6,6 @@ const CollectionPreview = ({title, items}) => (
         <h1 className='title'>{title.toUpperCase()}</h1>
         <div className='preview'>
             {
-                // We can chain together multiple array methods on a single array.
-                // Here we are using filter() to filter out 4 items from our array 
-                // and then mapping our values to create each item inside of the preview 
-                // collection component.
-
-                // The code below can be a performance issue because every time the component is rendered
-                // or re rendered these methods on the array will run. We will learn how to handle this
-                // better later on in the course.
                 items.filter((item, idx) => idx < 4)
                     .map((item) => (
                     <div key={item.id}>{item.name}</div>
