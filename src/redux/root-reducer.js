c26eee5289ac6c15b1730c983e4ec9436cf2d78a
dwilliams64 <dwilliams_64@yahoo.com>
2020-02-14 11:07:45 -0600

    Code clean up

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,14 +1,7 @@
-// The root reducer is the base reducer object that will combine all of states together.
-
-// Function from the redux lib that will combine all our reducers.
 import { combineReducers } from 'redux';
-
-// Importing user reducer.
 import userReducer from './user/user-reducer';
 
-// Combines all othe reducers into one giant object the manages state (object is the store)
 export default combineReducers ({
-    // Our user reducer property in this object.
     user: userReducer
 }); 
 
