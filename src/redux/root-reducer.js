3b11154d67e9f2db7b31f3fb2256cb4038be0235
dwilliams64 <dwilliams_64@yahoo.com>
2020-02-18 09:25:28 -0600

    Code clean up

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,6 @@
 import { combineReducers } from 'redux';
 import userReducer from './user/user-reducer';
 
-// Root Reducer.
-// Combines all reducers into one big object.
 export default combineReducers ({
     user: userReducer
 }); 
--- src/redux/user/user-reducer.js
+++ src/redux/user/user-reducer.js
@@ -2,12 +2,7 @@ const INTIAL_STATE = {
     currentUser: null
 }
 
-// User Reducer.
-// This reducer will handle user state in our app.
-// This reducer will pass its payload and state to the Root Reducer.
 const userReducer = (state = INTIAL_STATE, action) => {
-    // Used to check type. If type matches then this reducer returns state and payload to Root reducer.
-    // If type does not match current state is returned to the Root reducer.
     switch (action.type) {        
         case 'SET_CURRENT_USER':
             return {
