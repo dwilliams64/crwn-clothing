5204ef36453a20887ebee47e5cbe6484b0da4692
dwilliams64 <dwilliams_64@yahoo.com>
2020-02-20 08:32:22 -0600

    Implemented cart reducer

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+// Importing in action. This component recieves an action because we want
+// the user to click on the cart-icon component to trigger the hidding
+// showing of the cart drop down component.
+import { toggleCartHidden } from '../../redux/cart/cart.action';
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 import './cart-icon.styles.scss';
 
-const CartIcon = () => (
+const CartIcon = ({ toggleCartHidden }) => (
     <div>
-        <ShoppingIcon className='shopping-icon' />
+        <ShoppingIcon className='shopping-icon' onClick={toggleCartHidden}/>
         <span className='item-count'>0</span>
     </div>
 )
 
-export default CartIcon;
+const mapDispatchToProps = dispatch => ({
+    toggleCartHidden: () => dispatch(toggleCartHidden())
+});
+
+export default connect(
+    null, 
+    mapDispatchToProps
+)(CartIcon);
