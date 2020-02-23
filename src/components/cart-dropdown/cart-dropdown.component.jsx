bfc4a0dd2ce98335a0b2a5d65d99d6ce773a4845
dwilliams64 <dwilliams_64@yahoo.com>
2020-02-23 07:45:47 -0600

    Changed return for drop down component

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -12,13 +12,7 @@ import { createStructuredSelector } from 'reselect';
 
 import './cart-dropdown.styles.scss';
 
-const CartDropDown = ({ cartItems, history, dispatch }) => {
-
-    // If we look at the console we can see that we have access to the dispatch
-    // function as prop of this component.
-
-    console.log(dispatch)
-    return (
+const CartDropDown = ({ cartItems, history, dispatch }) => (    
     <div className="cart-dropdown">
         <div className="cart-items">            
             {   
@@ -34,27 +28,14 @@ const CartDropDown = ({ cartItems, history, dispatch }) => {
 
         <CustomButton onClick={() => {
             history.push('/checkout');
-            
-            // Knowing this we can directly pass in our toggleCartHidden action creator
-            // into our component using the dispatch function directly in our component.
             dispatch(toggleCartHidden());
-
-            // By doing this when our user clicks on go to cart and they are redirected to the
-            // checkout page our toggleCartHidden() action creator is fired and the cart will
-            // be hidden.
-
-            // This setup of directly using dispatch inside of the component is good for when
-            // you just need to use dispatch once for something small like this.
         }}
         >GO TO CHECKOUT</CustomButton>
     </div>    
-)};
+);
 
 const mapStateToProps = createStructuredSelector({
     cartItems: selectCartItems
 });
 
-
-// If we do not supply a second argument (mapDispatchToProps) into connnect() then the dispatch
-// function is automatically passed into our component as a prop.
 export default withRouter(connect(mapStateToProps)(CartDropDown));
