1c9ad1da9e8e12a2d8377360a686b1d5f53d7a1e
dwilliams64 <dwilliams_64@yahoo.com>
2020-02-24 08:29:09 -0600

    Removed comments

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,4 @@
 import { createStore, applyMiddleware } from 'redux';
-
-// Allows our browser to cache our store depending on certian config options.
 import { persistStore } from 'redux-persist';
 
 import logger from 'redux-logger';
@@ -11,8 +9,6 @@ const middlewares = [logger];
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-// Passing in our store so we can cache it into either session storage or local storage.
-// This will persist our store.
 const persistor = persistStore(store);
 
 export { store, persistor };
