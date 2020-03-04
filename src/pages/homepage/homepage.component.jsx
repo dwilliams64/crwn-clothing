708b81122a8695b3768970862d40667b8435585d
dwilliams64 <dwilliams_64@yahoo.com>
2020-03-03 18:29:55 -0600

    Converted header and home page component to use styled components

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,13 +1,8 @@
 import React from 'react';
 import Directory from '../../components/directory/directory.component';
-
-// Importing our styled component from homepage.styles.jsx
 import { HomePageContainer } from './homepage.styles';
 
 const Homepage = () => (
-
-    // This component will render out a div with our styles we 
-    // defined inside of homepage.styles.jsx.
     <HomePageContainer>
         <Directory />
     </HomePageContainer>
--- src/pages/homepage/homepage.styles.jsx
+++ src/pages/homepage/homepage.styles.jsx
@@ -1,16 +1,8 @@
-// Importing styled components from lib
 import styled from 'styled-components';
 
-// Creating styled component called HomepageContainer
-// styled is from the styled-components lib
-// div is the type of HTML element we wish to render
-// We then place our styles in between ``
-// We can use any css property
-// It is like writing CSS not camel case.
 export const HomePageContainer = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
     padding: 20px 80px;
-
 `
