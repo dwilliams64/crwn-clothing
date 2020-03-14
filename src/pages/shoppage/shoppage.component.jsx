3303ad0eca5848e212515c0138f0ccbc40281121
dwilliams64 <dwilliams_64@yahoo.com>
2020-03-14 18:26:42 -0500

    Added notes

--- src/pages/shoppage/shoppage.component.jsx
+++ src/pages/shoppage/shoppage.component.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+
+// Importing connect component from react-redux
 import { connect } from 'react-redux';
 
 import CollectionPage from '../collection/collectionpage';
@@ -8,17 +10,26 @@ import CollectoinsOverview from '../../components/collections-overview/collectio
 
 import { firestore, convertCollectionsSnapshotToMap} from '../../firebase/firebase.utiles';
 
+// Importing updateCollections action from shop actions
 import { updateCollections } from '../../redux/shop/shop.actions';
 
 class ShopPage extends React.Component {
     unsubscribeFromSnapshot = null;
 
     componentDidMount() {
+
+        // Brings in a prop that will take our collections that we brought in from 
+        // firebase and bring that data into redux.
         const { updateCollections } = this.props;
+
         const collectionRef = firestore.collection('collections');
   
         this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
             const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+
+            // collections from firebase is passed into the prop (passed into the 
+            // props method) so that
+            // the collections can be brought into redux.
             updateCollections(collectionsMap);          
         });
     }
